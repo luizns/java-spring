1ae52a47d6ae11476c1ddbba278075cab82f2b4f
Luiz Nascimento <luns.wm@hotmail.com>
2024-10-07 22:35:10 -0300

    feat: Payment

--- dscommerce/src/main/java/com/github/luizns/dscommerce/entities/User.java
+++ dscommerce/src/main/java/com/github/luizns/dscommerce/entities/User.java
@@ -1,6 +1,7 @@
 package com.github.luizns.dscommerce.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tb_user")
 public class User {
     @Id
