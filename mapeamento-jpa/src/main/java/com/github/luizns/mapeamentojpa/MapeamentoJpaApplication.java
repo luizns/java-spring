69c1829495167723a500a87b98ebde14e573ae62
Luiz Nascimento <luns.wm@hotmail.com>
2024-11-15 20:12:26 -0300

    Initial commit

---
+++ mapeamento-jpa/src/main/java/com/github/luizns/mapeamentojpa/MapeamentoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.github.luizns.mapeamentojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentoJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapeamentoJpaApplication.class, args);
+    }
+
+}
---
+++ mapeamento-jpa/src/test/java/com/github/luizns/mapeamentojpa/MapeamentoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.luizns.mapeamentojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapeamentoJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
