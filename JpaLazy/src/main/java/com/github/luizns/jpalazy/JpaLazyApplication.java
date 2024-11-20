34e65e17ba31cf143aeff3ab21c5b1af11e0b1c5
Luiz Nascimento <luns.wm@hotmail.com>
2024-11-20 16:40:39 -0300

    Initial commit

---
+++ JpaLazy/src/main/java/com/github/luizns/jpalazy/JpaLazyApplication.java
@@ -0,0 +1,13 @@
+package com.github.luizns.jpalazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLazyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLazyApplication.class, args);
+    }
+
+}
---
+++ JpaLazy/src/test/java/com/github/luizns/jpalazy/JpaLazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.luizns.jpalazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLazyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
